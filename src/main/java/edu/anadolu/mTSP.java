83a237868dae27cab7a96221aa7849b361bc9856
Ahmet Arslan <iorixxx@yahoo.com>
2021-04-09 02:21:06 +0300

    update distance matrix

---
+++ src/main/java/edu/anadolu/mTSP.java
@@ -0,0 +1,20 @@
+package edu.anadolu;
+
+public class mTSP {
+
+    public mTSP(int depots, int salesmen) {
+    }
+
+    public void randomSolution() {
+    }
+
+    public void validate() {
+    }
+
+    public int cost() {
+        return Integer.MAX_VALUE;
+    }
+
+    public void print(boolean verbose) {
+    }
+}
